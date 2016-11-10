3a711d56615ea72988b277a743d28883ef024581
Prakash Satya <PrakashSatya@JohnDeere.com>
2016-11-09 20:10:35 -0600

    initial checkin

---
+++ src/main/java/com/pyxius/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pyxius.helloworld;
+
+/**
+ * Created by sp71427 on 11/9/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world from main...");
+    }
+}
